b03e50af031dbdf1960a3e3b8fc7587a8ca3c0be
Daniel Kuzminski <dankuzmina@gmail.com>
2024-07-13 20:01:44 +0200

    main page and navbar works in progress

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+//react
+import { useState } from 'react'
+
+//styles
+import './navbar.css'
+
+//assets
+import logo from '../../assets/logo.png'
+import ham from '../../assets/ham.png'
+
+//router
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    const [show, setShow] = useState(false)
+
+  return (
+    <nav className='navbar'>
+        <div className="logo-container">
+            <img src={logo} alt="" className="logo" />
+        </div>
+        <div className="nav-container">
+            <Link className="nav-link">Main</Link>
+            <Link className="nav-link">Band</Link>
+            <Link className="nav-link">Retail</Link>
+            <Link className="nav-link">Subscribe</Link>
+            <img src={ham} alt="" className='ham' />
+        </div>
+    </nav>
+  )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+//styles
+import './home.css'
+
+export default function Home() {
+  return (
+    <div className='home'>
+        <h1 className='home__title'>raiders of doom</h1>
+    </div>
+  )
+}
